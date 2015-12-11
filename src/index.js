2207bc334a6a7dd885acfc2a4de370bd1d13b804
Jan Henrik Overland <janhenrik.overland@gmail.com>
2015-12-11 16:07:35 +0100

    references

--- src/index.js
+++ src/index.js
@@ -11,10 +11,54 @@ import {dataApprovalLevelsInit} from './init/dataApprovalLevelsInit.js';
 
 // initialize
 function init() {
+
+    //TMP
+    manager.SessionStorageManager = function() {
+        var t = this;
+
+        // constants
+        t.db = 'dhis2';
+        t.supported = ('sessionStorage' in window && window['sessionStorage'] !== null);
+
+        // fn
+        t.supportHandler = function() {
+            if (!this.supported) {
+                alert("Your browser is outdated and does not support local storage. Please upgrade your browser.");
+                return;
+            }
+
+            return true;
+        };
+    };
+
+    manager.SessionStorageManager.prototype.has = function(session) {
+        if (!this.supportHandler()) {
+            return;
+        }
+
+        return (JSON.parse(sessionStorage.getItem(this.db)) && JSON.parse(sessionStorage.getItem(this.db))[session]);
+    };
+
+    manager.SessionStorageManager.prototype.set = function(layout, session, url) {
+        if (!this.supportHandler()) {
+            return;
+        }
+
+        var dhis2 = JSON.parse(sessionStorage.getItem(this.db)) || {};
+        dhis2[session] = layout;
+        sessionStorage.setItem(this.db, JSON.stringify(dhis2));
+
+        if (url) {
+            window.location.href = url;
+        }
+    };
+    //TMP
+
     var appManager = new manager.AppManager();
     var calendarManager = new manager.CalendarManager();
     var requestManager = new manager.RequestManager();
     var i18nManager = new manager.I18nManager();
+    var sessionStorageManager = new manager.SessionStorageManager();
 
     var manifestReq = $.getJSON('manifest.webapp');
     var systemInfoUrl = '/api/system/info.json';
@@ -35,6 +79,7 @@ function init() {
             });
         }
     };
+    //TMP
 
     manifestReq.done(function(manifest) {
         appManager.manifest = manifest;
@@ -77,11 +122,34 @@ function init() {
     // approval levels
     requestManager.add(new api.Request(dataApprovalLevelsInit(requestManager, appManager)));
 
+    requestManager.set(getTable);
     requestManager.run();
 
     });
     });
     });
+    });
+}
+
+function getTable() {
+    var response,colAxis,rowAxis,table;
+
+    var layout = new api.Layout({"id":"C0rhAq1oklh","name":"ANC: Coverages Quarterly","hideEmptyRows":false,"parentGraphMap":{"ImspTQPwCqd":""},"rowSubTotals":false,"displayDensity":"NORMAL","completedOnly":false,"colTotals":true,"showDimensionLabels":false,"sortOrder":0,"fontSize":"NORMAL","topLimit":0,"aggregationType":"DEFAULT","displayName":"ANC: Coverages Quarterly","colSubTotals":false,"showHierarchy":false,"rowTotals":true,"cumulative":false,"digitGroupSeparator":"SPACE","regression":false,"skipRounding":false,"reportParams":{"paramGrandParentOrganisationUnit":false,"paramReportingPeriod":false,"paramParentOrganisationUnit":false,"paramOrganisationUnit":true},"attributeDimensions":[],"programIndicatorDimensions":[],"attributeValues":[],"dataDimensionItems":[{"dataDimensionItemType":"INDICATOR","indicator":{"id":"Uvn6LCg7dVU","name":"ANC 1 Coverage","code":"IN_52486","created":"2012-11-13T12:51:32.215+0000","lastUpdated":"2015-04-10T14:01:56.760+0000"}},{"dataDimensionItemType":"INDICATOR","indicator":{"id":"OdiHJayrsKo","name":"ANC 2 Coverage","code":"IN_52491","created":"2012-11-13T12:51:38.949+0000","lastUpdated":"2015-04-10T14:02:17.622+0000"}},{"dataDimensionItemType":"INDICATOR","indicator":{"id":"sB79w2hiLp8","name":"ANC 3 Coverage","created":"2012-11-13T12:51:45.321+0000","lastUpdated":"2015-04-10T14:32:25.625+0000"}}],"columns":[{"dimension":"dx","items":[{"id":"Uvn6LCg7dVU","name":"ANC 1 Coverage"},{"id":"OdiHJayrsKo","name":"ANC 2 Coverage"},{"id":"sB79w2hiLp8","name":"ANC 3 Coverage"}]}],"dataElementDimensions":[],"categoryDimensions":[],"filters":[],"rows":[{"dimension":"ou","items":[{"id":"ImspTQPwCqd","name":"Sierra Leone"}]},{"dimension":"pe","items":[{"id":"LAST_4_QUARTERS","name":"LAST_4_QUARTERS"}]}],"categoryOptionGroups":[]});
+    var reqMap = layout.data();
+
+    reqMap.metaData.done(function(md) {
+        console.log("md", md);
+
+        reqMap.data.done(function(json) {
+            json.metaData = md.metaData;
+            console.log("json", json);
+
+            response = new api.Response(json);
+            colAxis = new pivot.TableAxis(layout, response, 'col');
+            rowAxis = new pivot.TableAxis(layout, response, 'row');
+            table = new pivot.Table(layout, response, colAxis, rowAxis);
+            document.body.innerHTML = table.html;
+        });
     });
 }
 
