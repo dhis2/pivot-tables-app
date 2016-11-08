c1f699a7728b266c445b728b6e4f550fca93fbaf
Jan Henrik Overland <janhenrik.overland@gmail.com>
2016-11-08 14:22:16 +0100

    plugin improvements

--- src/plugin.js
+++ src/plugin.js
@@ -48,7 +48,7 @@ refs.requestManager = requestManager;
 var i18nManager = new manager.I18nManager(refs);
 refs.i18nManager = i18nManager;
 
-// sessionstorage manager
+// session storage manager
 var sessionStorageManager = new manager.SessionStorageManager(refs);
 refs.sessionStorageManager = sessionStorageManager;
 
@@ -66,33 +66,131 @@ optionConfig.applyTo([].concat(arrayTo(api), arrayTo(pivot)));
 var Plugin = function() {
     var t = this;
 
-    var _isLoaded = false;
-
     t.url = null;
     t.username = null;
     t.password = null;
     t.spinner = false;
 
-    t.load = function(...layouts) {
-        if (!layouts.length) {
-            return;
-        }
+    var _isPending = false;
+    var _isReady = false;
 
+    var _layouts = [];
+
+    t.add = function(...layouts) {
         layouts = isArray(layouts[0]) ? layouts[0] : layouts;
 
-        _initialize(layouts);
+        if (layouts.length) {
+            _layouts = [..._layouts, ...layouts];
+        }
     };
 
-    var _initialize = function(layouts) {
-        if (!layouts.length) {
-            return;
+    t.load = function(...layouts) {
+        t.add(isArray(layouts[0]) ? layouts[0] : layouts);
+
+        _run();
+    };
+
+    var _run = function() {
+        if (_isReady) {
+            while (_layouts.length) {
+                _load(_layouts.shift());
+            }
+        }
+        else if (!_isPending) {
+            _isPending = true;
+
+            _initialize();
+        }
+    };
+
+    var _load = function(layout) {
+        if (t.spinner) {
+            $('#' + layout.el).append('<div class="spinner"></div>');
         }
 
-        if (_isLoaded) {
-            _load(layouts);
-            return;
-        }
+        var instanceRefs = {
+            dimensionConfig,
+            optionConfig,
+            periodConfig,
+            api,
+            pivot,
+            appManager,
+            calendarManager,
+            requestManager,
+            sessionStorageManager
+        };
 
+        var uiManager = new manager.UiManager(instanceRefs);
+        instanceRefs.uiManager = uiManager;
+        uiManager.applyTo(arrayTo(api));
+
+        var instanceManager = new manager.InstanceManager(instanceRefs);
+        instanceRefs.instanceManager = instanceManager;
+        instanceManager.apiResource = 'reportTable';
+        instanceManager.apiEndpoint = 'reportTables';
+        instanceManager.apiModule = 'dhis-web-pivot';
+        instanceManager.plugin = true;
+        instanceManager.dashboard = reportTablePlugin.dashboard;
+        instanceManager.applyTo(arrayTo(api));
+
+        var tableManager = new manager.TableManager(instanceRefs);
+        instanceRefs.tableManager = tableManager;
+
+            // instance manager
+        uiManager.setInstanceManager(instanceManager);
+
+        instanceManager.setFn(function(_layout) {
+            var sortingId = _layout.sorting ? _layout.sorting.id : null,
+                html = '',
+                table;
+
+            // get table
+            var getTable = function() {
+                var response = _layout.getResponse();
+                var colAxis = new pivot.TableAxis(_layout, response, 'col');
+                var rowAxis = new pivot.TableAxis(_layout, response, 'row');
+                return new pivot.Table(_layout, response, colAxis, rowAxis, {skipTitle: true});
+            };
+
+            // pre-sort if id
+            if (sortingId && sortingId !== 'total') {
+                _layout.sort();
+            }
+
+            // table
+            table = getTable();
+
+            // sort if total
+            if (sortingId && sortingId === 'total') {
+                _layout.sort(table);
+                table = getTable();
+            }
+
+            html += reportTablePlugin.showTitles ? uiManager.getTitleHtml(_layout.title || _layout.name) : '';
+            html += table.html;
+
+            uiManager.update(html, _layout.el);
+
+            // events
+            tableManager.setColumnHeaderMouseHandlers(_layout, table);
+
+            // mask
+            uiManager.unmask();
+        });
+
+        if (layout.id) {
+            instanceManager.getById(layout.id, function(_layout) {
+                _layout = new api.Layout(instanceRefs, objectApplyIf(layout, _layout));
+
+                instanceManager.getReport(_layout);
+            });
+        }
+        else {
+            instanceManager.getReport(new api.Layout(instanceRefs, layout));
+        }
+    };
+
+    var _initialize = function() {
         appManager.manifestVersion = VERSION;
         appManager.path = t.url;
         appManager.setAuth(t.username && t.password ? t.username + ':' + t.password : null);
@@ -104,98 +202,11 @@ var Plugin = function() {
             requestManager.add(new api.Request(init.legendSetsInit(refs)));
             requestManager.add(new api.Request(init.dimensionsInit(refs)));
 
-            _isLoaded = true;
+            _isReady = true;
+            _isPending = false;
 
-            requestManager.set(_load, layouts);
+            requestManager.set(_run);
             requestManager.run();
-        });
-    };
-
-    var _load = function(layouts) {
-        layouts.forEach(function(layout) {
-            if (t.spinner) {
-                $('#' + layout.el).append('<div class="spinner"></div>');
-            }
-
-            var instanceRefs = {
-                dimensionConfig,
-                optionConfig,
-                periodConfig,
-                api,
-                pivot,
-                appManager,
-                calendarManager,
-                requestManager,
-                sessionStorageManager
-            };
-
-            var uiManager = new manager.UiManager(instanceRefs);
-            instanceRefs.uiManager = uiManager;
-            uiManager.applyTo(arrayTo(api));
-
-            var instanceManager = new manager.InstanceManager(instanceRefs);
-            instanceRefs.instanceManager = instanceManager;
-            instanceManager.apiResource = 'reportTable';
-            instanceManager.apiEndpoint = 'reportTables';
-            instanceManager.apiModule = 'dhis-web-pivot';
-            instanceManager.plugin = true;
-            instanceManager.dashboard = reportTablePlugin.dashboard;
-            instanceManager.applyTo(arrayTo(api));
-
-            var tableManager = new manager.TableManager(instanceRefs);
-            instanceRefs.tableManager = tableManager;
-
-                // instance manager
-            uiManager.setInstanceManager(instanceManager);
-
-            instanceManager.setFn(function(_layout) {
-                var sortingId = _layout.sorting ? _layout.sorting.id : null,
-                    html = '',
-                    table;
-
-                // get table
-                var getTable = function() {
-                    var response = _layout.getResponse();
-                    var colAxis = new pivot.TableAxis(_layout, response, 'col');
-                    var rowAxis = new pivot.TableAxis(_layout, response, 'row');
-                    return new pivot.Table(_layout, response, colAxis, rowAxis, {skipTitle: true});
-                };
-
-                // pre-sort if id
-                if (sortingId && sortingId !== 'total') {
-                    _layout.sort();
-                }
-
-                // table
-                table = getTable();
-
-                // sort if total
-                if (sortingId && sortingId === 'total') {
-                    _layout.sort(table);
-                    table = getTable();
-                }
-
-                html += reportTablePlugin.showTitles ? uiManager.getTitleHtml(_layout.title || _layout.name) : '';
-                html += table.html;
-
-                uiManager.update(html, _layout.el);
-
-                // events
-                tableManager.setColumnHeaderMouseHandlers(_layout, table);
-
-                // mask
-                uiManager.unmask();
-            });
-
-            if (layout.id) {
-                instanceManager.getById(layout.id, function(_layout) {
-                    _layout = new api.Layout(instanceRefs, objectApplyIf(layout, _layout));
-                    instanceManager.getReport(_layout);
-                });
-            }
-            else {
-                instanceManager.getReport(new api.Layout(instanceRefs, layout));
-            }
         });
     };
 };
